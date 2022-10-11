453078080ed30a39bc504eb10f86649176deb26f
mart6110 <martinus.mortensen@gmail.com>
2022-10-11 09:39:12 +0200

    Admin login & kontakt

---
+++ Pizza_Assingnment/Controllers/AdministrationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza_Assingnment.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: AdministrationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
